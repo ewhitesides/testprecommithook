1204182bba06d263e97281d7898d967c101ecd96
Erik Whitesides <eklevjer@gmail.com>
2023-10-04 14:47:07 +0000

    update postCreate script

--- .devcontainer/postCreateScript.sh
+++ .devcontainer/postCreateScript.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 echo 'PS1="$PS1\n> "' >> ~/.bashrc
 
-#install dependencies from setup.py, rm the stuff that is built
-pip3 install pre-commit
-#pip3 install --user -r requirements.txt
+#in the devcontainer pip is same as pip3
+pip install --upgrade pip
+pip install pre-commit
+
+#install other requirements
+#pip install --user -r requirements.txt
